ab43daaaf799f3556b8c796985a54219187bb5b9
Harshita Arora <harshita.arora.fbd@gmail.com>
2026-03-28 13:51:17 +0530

    Added detection logic

--- src/main.py
+++ src/main.py
@@ -1,30 +1,14 @@
-import re
-
 def read_logs(file_path):
     with open(file_path, 'r') as file:
-        return file.readlines()
-
-def parse_log(log):
-    pattern = r'(\d+\.\d+\.\d+\.\d+).*"(\w+) (.*?) HTTP.*" (\d{3})'
-    match = re.search(pattern, log)
-
-    if match:
-        return {
-            "ip" : match.group(1),
-            "method" : match.group(2),
-            "endpoint" : match.group(3),
-            "status" : match.group(4)
-        }
-    return None
+        logs = file.readlines()
+    return logs
 
 def main():
     file_path = "logs/sample.log"
     logs = read_logs(file_path)
 
     for log in logs:
-        parsed = parse_log(log)
-        if parsed:
-            print(parsed)
+        print(log.strip())
 
 if __name__ == "__main__":
     main()
